97eda57492b7478f25eca020da74b4486a1e16b6
AdrianIgl <adrian.igldel@educa.jcyl.es>
2020-11-09 17:33:14 +0100

    prueba

--- index.php
+++ index.php
@@ -6,13 +6,31 @@ use Foolz\Inet\Inet;
 $client = new IPinfo();
 $num = new Inet();
 
+echo "<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Ipv4</title>
+</head>
+<body>
+<form  method='Post'>
+<p>Escribe tu numero</p>
+<input type='text' name='ip' />
+<input type='submit'value='Calcular' />
+</form>
+
+</form>
+</body>
+</html>";
+if (isset($_REQUEST['Calcular'])){
+
 
 $decimal_ip = $num->ptod("127.0.0.1");
 $ip = $num->dtop($decimal_ip);
 
-print($decimal_ip);
+print("Decimal =$decimal_ip");
 print("<br>");
-print($ip);
+print("Ip=$ip");
 print("<br>");
 
 
@@ -21,13 +39,25 @@ $ip_address = '213.177.221.24';
 $details = $client->getDetails($ip_address);
 
 echo "Ciudad = $details->city";
+print("<br>");
 echo "Pais = $details->country";
-echo "Ciudad = $details->city";
+print("<br>");
+echo "Nombre del Pais = $details->country_name";
+print("<br>");
+echo "Localizacion = $details->loc";
+print("<br>");
+echo "Codigo Postal = $details->postal";
+print("<br>");
+echo "Zona Horaria = $details->timezone";
+print("<br>");
+echo "Region = $details->region";
+print("<br>");
+echo "Latitud = $details->latitude";
+print("<br>");
+echo "Longitud = $details->longitude";
+print("<br>");
+}
+?>
 
 
 
-
-
-echo "<pre>";
-var_dump($details);
-echo "</pre>";
